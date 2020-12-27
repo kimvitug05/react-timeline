7849143b4b53a7d65c8132c4fb4790a89d61e4ee
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-26 16:48:46 -0800

    implement Timeline component and render in App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -11,9 +12,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{ timelineData.person }'s Social Media Feed</h1>
       </header>
       <main className="App-main">
+        <Timeline events={ timelineData.events } />
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const timelineEvents = props.events.map((event, i) => {
+    return (
+      <TimelineEvent key={i} person={ event.person } status={ event.status } timestamp={ event.timeStamp }/>
+    );
+  });
+
+  return (
+    <div>{ timelineEvents }</div>
+  );
 }
 
 export default Timeline;
