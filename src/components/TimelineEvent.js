e9d552621796691a491f40030130c6babfc720e8
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-26 16:47:29 -0800

    Implement TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>{ props.person }</li>
+        <li>{ props.status }</li>
+        <Timestamp time={ props.timeStamp } />
+      </ul>
+    </div>
+  );
 }
 
 export default TimelineEvent;
